5487d67b34b1b001c0a1f1a9bc7787bfdd90b19b
Nathan Darnell <darnell.nathaniel95@gmail.com>
2020-07-14 21:57:53 -0700

    fixed employee.json path may need to put employee mapping in its own component to pass down to the table

--- employee_directory/src/components/Table/index.js
+++ employee_directory/src/components/Table/index.js
@@ -1,17 +1,15 @@
 import React from "react";
 import "./style.css";
 // import Table from 'react-bootstrap/Table'
-import employees from "./employee.json"
+import employees from "../../employee.json"
 
 function TableOfPeople() {
   return (
     // need to map to employee.json 
-    // follow activity 12 for props example may not be able to map this way.
-    <div>
+    // follow activity 12 for props example may not be able to map this way. also see activity 13
       <ul>
         {employees.map(items => (<li key={items.id}>{items.name}</li>))}
       </ul>
-    </div>
   );
 }
 
