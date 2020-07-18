df7fc03a00226ebdac5a90c691d98bfe15c64de7
Nathan Darnell <darnell.nathaniel95@gmail.com>
2020-07-17 17:35:35 -0700

    added search component

---
+++ employee_directory/src/components/SearchForm/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Form from "react-bootstrap/Form"
+import "./style.css";
+
+const SearchBar = ({search, handleInputChange})=>{
+    return(
+        <Form className="text-center">
+            <Form.Group>
+                <Form.Label>Search for the character youre looking for!</Form.Label>
+                <Form.Control 
+                    type="text" 
+                    name="search" 
+                    value={search} 
+                    onChange={handleInputChange} 
+                    placeholder="Search Directory!">
+                </Form.Control>
+            </Form.Group>
+        </Form>
+    )
+}
+
+export default SearchBar;
+
+
