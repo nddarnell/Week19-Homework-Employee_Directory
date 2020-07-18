968c4545e39bb17bac8c8c166d239c565c5030af
Nathan Darnell <darnell.nathaniel95@gmail.com>
2020-07-18 14:25:58 -0700

    added search functionality and mapped people instead of hard coding

--- employee_directory/src/components/TableHeader/index.js
+++ employee_directory/src/components/TableHeader/index.js
@@ -23,20 +23,25 @@ class TableHeader extends Component {
         console.log(this.state)
     };
 
-    handleInputChange = event =>{
+    handleInputChange = event => {
         event.preventDefault()
-        const {name, value} = event.target;
+        const { name, value } = event.target;
 
         this.setState({
             [name]: value
         });
         console.log(this.state)
 
-        const filter = this.state.default.filter(employees =>{
-            return(employees.name)
+        // const result = words.filter(word => word.length > 6);
+
+        const filter = this.state.default.filter(employees => {
+
+            // console.log(employees.name.includes(this.state.name))
+            return (employees.name.includes(this.state.search))
         });
 
-        this.setState({results: filter})
+        this.setState({ result: filter })
+        console.log(this.state.result)
     }
 
     render() {
@@ -54,83 +59,16 @@ class TableHeader extends Component {
                             <th>Date of Birth</th>
                         </tr>
                     </thead>
-                    <TableOfPeople
-                        name={employees[0].name}
-                        image={employees[0].image}
-                        phone={employees[0].phone}
-                        email={employees[0].email}
-                        dob={employees[0].dob}
-                    />
-                    <TableOfPeople
-                        name={employees[1].name}
-                        image={employees[1].image}
-                        phone={employees[1].phone}
-                        email={employees[1].email}
-                        dob={employees[1].dob}
-                    />
-                    <TableOfPeople
-                        name={employees[2].name}
-                        image={employees[2].image}
-                        phone={employees[2].phone}
-                        email={employees[2].email}
-                        dob={employees[2].dob}
-                    />
-                    <TableOfPeople
-                        name={employees[3].name}
-                        image={employees[3].image}
-                        phone={employees[3].phone}
-                        email={employees[3].email}
-                        dob={employees[3].dob}
-                    />
-                    <TableOfPeople
-                        name={employees[4].name}
-                        image={employees[4].image}
-                        phone={employees[4].phone}
-                        email={employees[4].email}
-                        dob={employees[4].dob}
-                    />
-                    <TableOfPeople
-                        name={employees[5].name}
-                        image={employees[5].image}
-                        phone={employees[5].phone}
-                        email={employees[5].email}
-                        dob={employees[5].dob}
-                    />
-                    <TableOfPeople
-                        name={employees[6].name}
-                        image={employees[6].image}
-                        phone={employees[6].phone}
-                        email={employees[6].email}
-                        dob={employees[6].dob}
-                    />
-                    <TableOfPeople
-                        name={employees[7].name}
-                        image={employees[7].image}
-                        phone={employees[7].phone}
-                        email={employees[7].email}
-                        dob={employees[7].dob}
-                    />
-                    <TableOfPeople
-                        name={employees[8].name}
-                        image={employees[8].image}
-                        phone={employees[8].phone}
-                        email={employees[8].email}
-                        dob={employees[8].dob}
-                    />
-                    <TableOfPeople
-                        name={employees[9].name}
-                        image={employees[9].image}
-                        phone={employees[9].phone}
-                        email={employees[9].email}
-                        dob={employees[9].dob}
-                    />
-                    <TableOfPeople
-                        name={employees[10].name}
-                        image={employees[10].image}
-                        phone={employees[10].phone}
-                        email={employees[10].email}
-                        dob={employees[10].dob}
-                    />
+                    {this.state.result.map(people => (
+                        <TableOfPeople
+                            key={people.id}
+                            name={people.name}
+                            image={people.image}
+                            phone={people.phone}
+                            email={people.email}
+                            dob={people.dob}
+                        />
+                    ))}
                 </Table>
 
             </>
